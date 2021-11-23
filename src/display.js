f2b6f039adaf0f00f00e3fefc6e4f56155cebeb0
Diana0604 <diana.vallverdu@gmail.com>
2021-11-23 16:11:04 +0000

    move signatures to subfolder

--- src/display.js
+++ src/display.js
@@ -6,13 +6,13 @@ const ipc = require('electron').ipcRenderer;
 
 function display(display_type, elements){
     if(display_type == constants.HTML){
-        ipc.send('load-page', {page: './src/only_html.html', data: {name: elements[0].value, role: elements[1].value, phone: elements[2].value, email: elements[3].value}});
+        ipc.send('load-page', {page: './src/html/DC/only_html.html', data: {name: elements[0].value, role: elements[1].value, phone: elements[2].value, email: elements[3].value}});
     }
     if(display_type == constants.OUTLOOK){
-        ipc.send('load-page', {page:'./src/outlook.html', data: {name: elements[0].value, role: elements[1].value, phone: elements[2].value, email: elements[3].value}});
+        ipc.send('load-page', {page:'./src/html/DC/outlook.html', data: {name: elements[0].value, role: elements[1].value, phone: elements[2].value, email: elements[3].value}});
     }
     if(display_type == constants.GMAIL){
-        ipc.send('load-page', {page:'./src/signature.html', data: {name: elements[0].value, role: elements[1].value, phone: elements[2].value, email: elements[3].value}});
+        ipc.send('load-page', {page:'./src/html/DC/signature.html', data: {name: elements[0].value, role: elements[1].value, phone: elements[2].value, email: elements[3].value}});
     }
 }
 
