95ea464f816bf0f3e614e2e59e99948cb97f34b6
Diana0604 <diana.vallverdu@gmail.com>
2022-02-28 09:53:51 +0000

    compile only one signature

--- src/display.js
+++ src/display.js
@@ -3,31 +3,16 @@ const fs = require('fs');
 
 // In renderer process (web page).
 const ipc = require('electron').ipcRenderer;
-var select=document.getElementById('company');
 
 function display(display_type, elements){
-    if(select.value=="DC"){
-        if(display_type == constants.HTML){
-            ipc.send('load-page', {page: './src/html/DC/only_html.html', data: {name: elements[0].value, role: elements[1].value, phone: elements[2].value, email: elements[3].value}});
-        }
-        if(display_type == constants.OUTLOOK){
-            ipc.send('load-page', {page:'./src/html/DC/outlook.html', data: {name: elements[0].value, role: elements[1].value, phone: elements[2].value, email: elements[3].value}});
-        }
-        if(display_type == constants.GMAIL){
-            ipc.send('load-page', {page:'./src/html/DC/signature.html', data: {name: elements[0].value, role: elements[1].value, phone: elements[2].value, email: elements[3].value}});
-        }
+    if(display_type == constants.HTML){
+        ipc.send('load-page', {page: './src/html/only_html.html', data: {name: elements[0].value, role: elements[1].value, phone: elements[2].value, email: elements[3].value}});
     }
-    
-    if(select.value=="EB"){
-        if(display_type == constants.HTML){
-            ipc.send('load-page', {page: './src/html/EB/only_html.html', data: {name: elements[0].value, role: elements[1].value, phone: elements[2].value, email: elements[3].value}});
-        }
-        if(display_type == constants.OUTLOOK){
-            ipc.send('load-page', {page:'./src/html/EB/outlook.html', data: {name: elements[0].value, role: elements[1].value, phone: elements[2].value, email: elements[3].value}});
-        }
-        if(display_type == constants.GMAIL){
-            ipc.send('load-page', {page:'./src/html/EB/signature.html', data: {name: elements[0].value, role: elements[1].value, phone: elements[2].value, email: elements[3].value}});
-        }
+    if(display_type == constants.OUTLOOK){
+        ipc.send('load-page', {page:'./src/html/outlook.html', data: {name: elements[0].value, role: elements[1].value, phone: elements[2].value, email: elements[3].value}});
+    }
+    if(display_type == constants.GMAIL){
+        ipc.send('load-page', {page:'./src/html/signature.html', data: {name: elements[0].value, role: elements[1].value, phone: elements[2].value, email: elements[3].value}});
     }
 }
 
