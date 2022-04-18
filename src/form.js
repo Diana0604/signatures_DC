bce2d14debedf6ed2c9e5711c6c140a1077f220c
Diana0604 <diana.vallverdu@gmail.com>
2022-04-18 09:28:09 +0100

    form formatted

--- src/form.js
+++ src/form.js
@@ -1,24 +1,24 @@
-const constants = require('./constants');
-const display = require('./display');
+const constants = require("./constants");
+const display = require("./display");
 
-let form = document.getElementById('person_data');
+let form = document.getElementById("person_data");
 
 let output_type = null;
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    display.display(output_type, form.elements);
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  display.display(output_type, form.elements);
 });
 
-let button_html = document.getElementById('button_html');
-button_html.addEventListener('click', () => {
-    output_type = constants.HTML;
+let button_html = document.getElementById("button_html");
+button_html.addEventListener("click", () => {
+  output_type = constants.HTML;
 });
-let button_outlook = document.getElementById('button_outlook');
-button_outlook.addEventListener('click', () => {
-    output_type = constants.OUTLOOK;
+let button_outlook = document.getElementById("button_outlook");
+button_outlook.addEventListener("click", () => {
+  output_type = constants.OUTLOOK;
 });
-let button_gmail = document.getElementById('button_gmail');
-button_gmail.addEventListener('click', () => {
-    output_type = constants.GMAIL;
+let button_gmail = document.getElementById("button_gmail");
+button_gmail.addEventListener("click", () => {
+  output_type = constants.GMAIL;
 });
