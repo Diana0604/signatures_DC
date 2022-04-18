f78aab09feb8a7cd84957c2d523e1e007897dd52
Diana0604 <diana.vallverdu@gmail.com>
2022-04-18 09:27:33 +0100

    main formatted

--- main.js
+++ main.js
@@ -1,58 +1,55 @@
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow } = require("electron");
 
-const ipcMain = require('electron').ipcMain;
+const ipcMain = require("electron").ipcMain;
 
 let data;
 
 // in main process, outside of app.on:
-ipcMain.on('load-page', (event, arg) => {
-    
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false
-        }
-    });
-    win.loadFile(arg.page);
-    data = arg.data;
+ipcMain.on("load-page", (event, arg) => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+  win.loadFile(arg.page);
+  data = arg.data;
 });
 
-ipcMain.on('display-data', (event) => {
-    event.reply('data', data);
+ipcMain.on("display-data", (event) => {
+  event.reply("data", data);
 });
-
 
 //define method to create main window
-function createMainWindow(){
-    console.log(process.versions.node);
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false
-        }
-    });
+function createMainWindow() {
+  console.log(process.versions.node);
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
 
-    win.loadFile('index.html');
-    //win.webContents.openDevTools();
+  win.loadFile("index.html");
+  //win.webContents.openDevTools();
 }
 //create main window upon starting the app
 app.whenReady().then(createMainWindow);
 
 //if all windows closed on mac do not quit the app
-app.on('window-all-closed', () => {
-    if(process.platform != 'darwin') {
-        app.quit();
-    }
+app.on("window-all-closed", () => {
+  if (process.platform != "darwin") {
+    app.quit();
+  }
 });
 
-
 //if click on app icon on mac (not sure if has any impact elsewhere) and no windows open, create window
-app.on('activate', () => {
-    if(BrowserWindow.getAllWindows().length == 0){
-        createMainWindow();
-    }
-})
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length == 0) {
+    createMainWindow();
+  }
+});
